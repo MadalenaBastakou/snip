b246b7e99914ae6e72b4a439a263266b5dce6843
Madalena Bastakou <madalena.bastakou@gmail.com>
2024-03-14 11:04:26 +0200

    deploy route

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const __variableOfChoice = path.resolve()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "https://snip-xmnt.onrender.com"], credentials: true }));
 app.use(express.static(path.join(__variableOfChoice,  '/frontend/dist')));
 
 //user authentication paths
